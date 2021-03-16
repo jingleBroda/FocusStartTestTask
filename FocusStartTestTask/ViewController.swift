e0ecf6cdf6248514efc9454588eb81b5cb811627
Max <max@macbook-air-max.local>
2021-03-16 17:33:45 +0700

    Initial Commit

---
+++ FocusStartTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusStartTestTask
+//
+//  Created by Max on 16.03.2021.
+//  Copyright © 2021 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
